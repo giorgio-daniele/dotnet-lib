9f5ffb41194630b1adc57b879f329da87505b762
giorgiodaniele <giorgiodaniele15@gmail.com>
2025-07-08 09:00:51 +0200

    Alignment to standard definition

--- vars/pushQG.groovy
+++ vars/pushQG.groovy
@@ -1,14 +1,15 @@
-def call(Map args) {
+def call(Map config) {
 
-    def QA_RESULT                 = config.QA_RESULT
-    def APPLICATION_VERSION       = config.APPLICATION_VERSION
-    //def GITLAB_REPORTS_PROJECT_ID = config.GITLAB_REPORTS_PROJECT_ID
-    def gitlabReportUrl           = config.gitlabReportUrl
+    def QA_RESULT             = config.QA_RESULT
+    def APPLICATION_VERSION   = config.APPLICATION_VERSION
+    def gitlabReportUrl       = config.gitlabReportUrl
+    def gitlabReportGroup     = config.gitlabReportGroup
+    def gitlabReportProject   = config.gitlabReportProject
     
     if (!APPLICATION_VERSION.equals("dev")) {
 
-        def qaResult         = QA_RESULT["quality-assurance.result"]    ?: "N/A"
-        def sonarResult      = QA_RESULT["sonarqube.result"]            ?: "N/A"
+        def qaResult         = QA_RESULT["quality-assurance.esito"]     ?: "N/A"
+        def sonarResult      = QA_RESULT["sonarqube.esito"]             ?: "N/A"
         def sonarReportUrl   = QA_RESULT["sonarqube.report-url"]        ?: "N/A"
         def sonarDescription = QA_RESULT["sonarqube.description"]       ?: "N/A"
         def dtResult         = QA_RESULT["dependencytrack.result"]      ?: "N/A"
@@ -34,19 +35,34 @@ def call(Map args) {
             </metadata>
         """.stripIndent()
 
-        def timestamp      = new Date().format("yyyyMMdd_HHmmss")
-        def reportFileName = "${timestamp}_metadata.xml"
-        writeFile file: reportFileName, text: xmlContent
+        def timestamp = new Date().format("yyyyMMdd_HHmmss")
 
+        // Define the name of the file
+        def nameFile = "${timestamp}" + 
+                                  "/" +
+                    gitlabReportGroup + 
+                                  "/" + 
+                  gitlabReportProject + 
+                                  "/" + 
+                  APPLICATION_VERSION + 
+                                "/QG" + 
+                        "${qaResult}" + "_report"
+
+        // Write the XML content within the file                
+        writeFile file: nameFile, text: xmlContent
+
+        // Define the path on which posting the result
+        def projectPath  = gitlabReportGroup + "/" + gitlabReportProject
         def encodedPath  = java.net.URLEncoder.encode(projectPath, "UTF-8").replaceAll("%", "%%")
 
+        // Define the payload of the request
         def payload = [
             branch: "main",
-            commit_message: "Add file ${reportFileName}",
+            commit_message: "Add file ${nameFile}",
             actions: [[
                 action:    "create",
-                file_path: "reports/${reportFileName}",
-                content:   readFile(reportFileName)
+                file_path: "reports/${nameFile}",
+                content:    readFile(nameFile)
             ]]
         ]
         writeFile file: "payload.json", text: groovy.json.JsonOutput.toJson(payload)
@@ -55,15 +71,14 @@ def call(Map args) {
             @echo off
             setlocal
             set "PROJECT=${encodedPath}"
-            set "CMTS=${gitlabUrl}/api/v4/projects/%PROJECT%/repository/commits"
+            set "URL=${gitlabReportUrl}/api/v4/projects/%PROJECT%/repository/commits"
 
             curl -k                                       ^
                 --request POST                            ^
                 --header "PRIVATE-TOKEN: %TOKEN%"         ^
                 --header "Content-Type: application/json" ^
                 --data @payload.json                      ^
-                "%CMTS%" > response.json
-
+                "%URL%" > response.json
             type response.json
             endlocal
         """
